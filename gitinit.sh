4c8c917d82385b18cbd79f01263e72280b33d567
xiebaoma <2402583447@qq.com>
2026-01-02 14:02:37 +0800

    end

---
+++ gitinit.sh
@@ -0,0 +1,7 @@
+echo "# miniobreport" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/xiebaoma/miniobreport.git
+git push -u origin main
